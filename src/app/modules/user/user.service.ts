a0d5c03983bc3337be251a504f6e260c032b186c
ssmahim01 <mahimzayn666@gmail.com>
2026-04-23 19:37:27 +0600

    Updated service of user

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,122 +1,138 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import httpStatus from 'http-status-codes';
+import httpStatus from "http-status-codes";
 import { IUser, Role } from "./user.interface";
 import { User } from "./user.model";
-import AppError from '../../errorHelpers/appError';
+import AppError from "../../errorHelpers/appError";
 import bcryptjs from "bcryptjs";
-import { envVars } from '../../config/env';
-import { JwtPayload } from 'jsonwebtoken';
-import { QueryBuilder } from '../../utils/QueryBuilder';
-import { userSearchableFields } from './user.constants copy';
-import { Order } from '../order/order.model';
+import { envVars } from "../../config/env";
+import { JwtPayload } from "jsonwebtoken";
+import { QueryBuilder } from "../../utils/QueryBuilder";
+import { userSearchableFields } from "./user.constants copy";
+import { Order } from "../order/order.model";
 
 const createUserService = async (payload: Partial<IUser>) => {
-    const { email, password, ...rest } = payload;
+  const { email, password, ...rest } = payload;
 
-    const isExistUser = await User.findOne({ email })
+  const isExistUser = await User.findOne({ email });
 
-    if (isExistUser) {
-        throw new AppError(httpStatus.BAD_REQUEST, "User already exist")
-    }
+  if (isExistUser) {
+    throw new AppError(httpStatus.BAD_REQUEST, "User already exist");
+  }
 
-    const hashPassword = await bcryptjs.hash(password as string, 10)
-    const user = await User.create({
-        email,
-        password: hashPassword,
-        ...rest
-    })
+  const hashPassword = await bcryptjs.hash(password as string, 10);
+  const user = await User.create({
+    email,
+    password: hashPassword,
+    ...rest,
+  });
 
-    const { password: _, ...userWithoutPassword } = user.toObject();
+  const { password: _, ...userWithoutPassword } = user.toObject();
 
-    return userWithoutPassword;
-
-}
+  return userWithoutPassword;
+};
 
 const getMe = async (userId: string) => {
-    const user = await User.findById(userId).select("-password");
-    return {
-        data: user
-    }
+  const user = await User.findById(userId).select("-password");
+  return {
+    data: user,
+  };
 };
 
 const getSingleUser = async (id: string) => {
-    const user = await User.findById(id);
-    if (!user) {
-        throw new AppError(httpStatus.NOT_FOUND, "User Not Found")
-    }
-    return {
-        data: user
-    }
+  const user = await User.findById(id).populate("permissions");
+  if (!user) {
+    throw new AppError(httpStatus.NOT_FOUND, "User Not Found");
+  }
+  return {
+    data: user,
+  };
 };
 
 const updateUser = async (
-    userId: string,
-    payload: Partial<IUser>,
-    decodedToken: JwtPayload
+  userId: string,
+  payload: Partial<IUser>,
+  decodedToken: JwtPayload,
 ) => {
-    // Check if user exists
-    const existingUser = await User.findById(userId);
-    if (!existingUser) {
-        throw new AppError(httpStatus.NOT_FOUND, "User not found");
-    }
+  // Check if user exists
+  const existingUser = await User.findById(userId);
+  if (!existingUser) {
+    throw new AppError(httpStatus.NOT_FOUND, "User not found");
+  }
 
-    // Without Admin Role Restrictions
-    if (decodedToken.role !== Role.ADMIN) {
-        if (userId !== decodedToken.userId) {
-            throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized to update this user.");
-        }
-
-        if (payload.password) {
-            throw new AppError(httpStatus.BAD_REQUEST, "You cannot update your password here.");
-        }
-
-        if (payload.role || payload.isActive || payload.isDeleted || payload.isVerified) {
-            throw new AppError(httpStatus.FORBIDDEN, "You are not authorized to modify these fields.");
-        }
+  // Without Admin Role Restrictions
+  if (decodedToken.role !== Role.ADMIN) {
+    if (userId !== decodedToken.userId) {
+      throw new AppError(
+        httpStatus.UNAUTHORIZED,
+        "You are not authorized to update this user.",
+      );
     }
 
     if (payload.password) {
-        const hashedPassword = await bcryptjs.hash(payload.password, Number(envVars.BCRYPT_SALT_ROUND))
-        payload.password = hashedPassword;
+      throw new AppError(
+        httpStatus.BAD_REQUEST,
+        "You cannot update your password here.",
+      );
     }
 
-    // No restrictions for Admin — directly update
-    const updatedUser = await User.findByIdAndUpdate(userId, payload, {
-        new: true,
-        runValidators: true,
-    });
+    if (
+      payload.role ||
+      payload.isActive ||
+      payload.isDeleted ||
+      payload.isVerified
+    ) {
+      throw new AppError(
+        httpStatus.FORBIDDEN,
+        "You are not authorized to modify these fields.",
+      );
+    }
+  }
 
-    return updatedUser;
+  if (payload.password) {
+    const hashedPassword = await bcryptjs.hash(
+      payload.password,
+      Number(envVars.BCRYPT_SALT_ROUND),
+    );
+    payload.password = hashedPassword;
+  }
+
+  // No restrictions for Admin — directly update
+  const updatedUser = await User.findByIdAndUpdate(userId, payload, {
+    new: true,
+    runValidators: true,
+  });
+
+  return updatedUser;
 };
 
 const getMyCustomers = async (
   userId: string,
-  query: Record<string, string>
+  query: Record<string, string>,
 ) => {
-    const queryObj: any = {};
+  const queryObj: any = {};
 
-    // DATE FILTER
-    if (query["createdAt[gte]"] || query["createdAt[lte]"]) {
-        queryObj.createdAt = {};
+  // DATE FILTER
+  if (query["createdAt[gte]"] || query["createdAt[lte]"]) {
+    queryObj.createdAt = {};
 
-        if (query["createdAt[gte]"]) {
-            queryObj.createdAt.$gte = new Date(query["createdAt[gte]"]);
-        }
-
-        if (query["createdAt[lte]"]) {
-            queryObj.createdAt.$lte = new Date(query["createdAt[lte]"]);
-        }
+    if (query["createdAt[gte]"]) {
+      queryObj.createdAt.$gte = new Date(query["createdAt[gte]"]);
     }
 
-    // REMOVE SPECIAL FIELDS
-    delete query["createdAt[gte]"];
-    delete query["createdAt[lte]"];
+    if (query["createdAt[lte]"]) {
+      queryObj.createdAt.$lte = new Date(query["createdAt[lte]"]);
+    }
+  }
+
+  // REMOVE SPECIAL FIELDS
+  delete query["createdAt[gte]"];
+  delete query["createdAt[lte]"];
 
   const orders = await Order.find({
     seller: userId,
     isDeleted: false,
     isPublished: true,
-      ... queryObj,
+    ...queryObj,
   })
     .populate("customer", "name email phone address")
     .select("customer billingDetails");
@@ -126,9 +142,7 @@ const getMyCustomers = async (
   orders.forEach((order) => {
     if (order.customer?._id) {
       customerMap.set(order.customer._id.toString(), order.customer);
-    }
-
-    else if (order.billingDetails?.email) {
+    } else if (order.billingDetails?.email) {
       customerMap.set(order.billingDetails.email, {
         name: order.billingDetails.fullName,
         email: order.billingDetails.email,
@@ -149,216 +163,226 @@ const getMyCustomers = async (
 };
 
 const deleteUser = async (id: string) => {
-    const user = await User.findById(id);
-    if (!user) {
-        throw new AppError(httpStatus.NOT_FOUND, "User Not Found")
-    }
+  const user = await User.findById(id);
+  if (!user) {
+    throw new AppError(httpStatus.NOT_FOUND, "User Not Found");
+  }
 
-    await User.findByIdAndDelete(id);
+  await User.findByIdAndDelete(id);
 
-    return {
-        data: null
-    }
+  return {
+    data: null,
+  };
 };
 
 const getAllUsers = async (query: Record<string, string>) => {
-    const queryObj: any = {};
+  const queryObj: any = {};
 
-    // DATE FILTER
-    if (query["createdAt[gte]"] || query["createdAt[lte]"]) {
-        queryObj.createdAt = {};
+  // DATE FILTER
+  if (query["createdAt[gte]"] || query["createdAt[lte]"]) {
+    queryObj.createdAt = {};
 
-        if (query["createdAt[gte]"]) {
-            queryObj.createdAt.$gte = new Date(query["createdAt[gte]"]);
-        }
-
-        if (query["createdAt[lte]"]) {
-            queryObj.createdAt.$lte = new Date(query["createdAt[lte]"]);
-        }
+    if (query["createdAt[gte]"]) {
+      queryObj.createdAt.$gte = new Date(query["createdAt[gte]"]);
     }
 
-    // REMOVE SPECIAL FIELDS
-    delete query["createdAt[gte]"];
-    delete query["createdAt[lte]"];
-
-    const queryBuilder = new QueryBuilder(
-        User.find({ role: { $ne: "CUSTOMER" }, isDeleted: false, ...queryObj }),
-        query
-    );
-    const usersData = queryBuilder
-        .filter()
-        .search(userSearchableFields)
-        .sort()
-        .fields()
-        .paginate();
-
-    const [data, meta] = await Promise.all([
-        usersData.build(),
-        queryBuilder.getMeta()
-    ])
-
-    return {
-        data,
-        meta
+    if (query["createdAt[lte]"]) {
+      queryObj.createdAt.$lte = new Date(query["createdAt[lte]"]);
     }
+  }
+
+  // REMOVE SPECIAL FIELDS
+  delete query["createdAt[gte]"];
+  delete query["createdAt[lte]"];
+
+  const queryBuilder = new QueryBuilder(
+    User.find({ role: { $ne: "CUSTOMER" }, isDeleted: false, ...queryObj }),
+    query,
+  );
+  const usersData = queryBuilder
+    .filter()
+    .search(userSearchableFields)
+    .sort()
+    .fields()
+    .paginate();
+
+  const [data, meta] = await Promise.all([
+    usersData.build(),
+    queryBuilder.getMeta(),
+  ]);
+
+  return {
+    data,
+    meta,
+  };
 };
 const getAllTrashUsers = async (query: Record<string, string>) => {
-    const queryObj: any = {};
+  const queryObj: any = {};
 
-    // DATE FILTER
-    if (query["createdAt[gte]"] || query["createdAt[lte]"]) {
-        queryObj.createdAt = {};
+  // DATE FILTER
+  if (query["createdAt[gte]"] || query["createdAt[lte]"]) {
+    queryObj.createdAt = {};
 
-        if (query["createdAt[gte]"]) {
-            queryObj.createdAt.$gte = new Date(query["createdAt[gte]"]);
-        }
-
-        if (query["createdAt[lte]"]) {
-            queryObj.createdAt.$lte = new Date(query["createdAt[lte]"]);
-        }
+    if (query["createdAt[gte]"]) {
+      queryObj.createdAt.$gte = new Date(query["createdAt[gte]"]);
     }
 
-    // REMOVE SPECIAL FIELDS
-    delete query["createdAt[gte]"];
-    delete query["createdAt[lte]"];
-
-    const queryBuilder = new QueryBuilder(
-        User.find({ role: { $ne: "CUSTOMER" }, isDeleted: true, ...queryObj }),
-        query
-    );
-    const usersData = queryBuilder
-        .filter()
-        .search(userSearchableFields)
-        .sort()
-        .fields()
-        .paginate();
-
-    const [data, meta] = await Promise.all([
-        usersData.build(),
-        queryBuilder.getMeta()
-    ])
-
-    return {
-        data,
-        meta
+    if (query["createdAt[lte]"]) {
+      queryObj.createdAt.$lte = new Date(query["createdAt[lte]"]);
     }
+  }
+
+  // REMOVE SPECIAL FIELDS
+  delete query["createdAt[gte]"];
+  delete query["createdAt[lte]"];
+
+  const queryBuilder = new QueryBuilder(
+    User.find({ role: { $ne: "CUSTOMER" }, isDeleted: true, ...queryObj }),
+    query,
+  );
+  const usersData = queryBuilder
+    .filter()
+    .search(userSearchableFields)
+    .sort()
+    .fields()
+    .paginate();
+
+  const [data, meta] = await Promise.all([
+    usersData.build(),
+    queryBuilder.getMeta(),
+  ]);
+
+  return {
+    data,
+    meta,
+  };
 };
 
 const getAllCustomers = async (query: Record<string, string>) => {
-    const queryObj: any = {};
+  const queryObj: any = {};
 
-    // DATE FILTER
-    if (query["createdAt[gte]"] || query["createdAt[lte]"]) {
-        queryObj.createdAt = {};
+  // DATE FILTER
+  if (query["createdAt[gte]"] || query["createdAt[lte]"]) {
+    queryObj.createdAt = {};
 
-        if (query["createdAt[gte]"]) {
-            queryObj.createdAt.$gte = new Date(query["createdAt[gte]"]);
-        }
-
-        if (query["createdAt[lte]"]) {
-            queryObj.createdAt.$lte = new Date(query["createdAt[lte]"]);
-        }
+    if (query["createdAt[gte]"]) {
+      queryObj.createdAt.$gte = new Date(query["createdAt[gte]"]);
     }
 
-    // REMOVE SPECIAL FIELDS
-    delete query["createdAt[gte]"];
-    delete query["createdAt[lte]"];
-
-    const queryBuilder = new QueryBuilder(
-        User.find({ role: "CUSTOMER", isDeleted: false, ...queryObj }),
-        query
-    );
-
-    const usersData = queryBuilder
-        .filter()
-        .search(userSearchableFields)
-        .sort()
-        .fields()
-        .paginate();
-
-    const [data, meta] = await Promise.all([
-        usersData.build(),
-        queryBuilder.getMeta()
-    ])
-
-    return {
-        data,
-        meta
+    if (query["createdAt[lte]"]) {
+      queryObj.createdAt.$lte = new Date(query["createdAt[lte]"]);
     }
+  }
+
+  // REMOVE SPECIAL FIELDS
+  delete query["createdAt[gte]"];
+  delete query["createdAt[lte]"];
+
+  const queryBuilder = new QueryBuilder(
+    User.find({ role: "CUSTOMER", isDeleted: false, ...queryObj }),
+    query,
+  );
+
+  const usersData = queryBuilder
+    .filter()
+    .search(userSearchableFields)
+    .sort()
+    .fields()
+    .paginate();
+
+  const [data, meta] = await Promise.all([
+    usersData.build(),
+    queryBuilder.getMeta(),
+  ]);
+
+  return {
+    data,
+    meta,
+  };
 };
 
 const getAllTrashCustomers = async (query: Record<string, string>) => {
-    const queryObj: any = {};
+  const queryObj: any = {};
 
-    // DATE FILTER
-    if (query["createdAt[gte]"] || query["createdAt[lte]"]) {
-        queryObj.createdAt = {};
+  // DATE FILTER
+  if (query["createdAt[gte]"] || query["createdAt[lte]"]) {
+    queryObj.createdAt = {};
 
-        if (query["createdAt[gte]"]) {
-            queryObj.createdAt.$gte = new Date(query["createdAt[gte]"]);
-        }
-
-        if (query["createdAt[lte]"]) {
-            queryObj.createdAt.$lte = new Date(query["createdAt[lte]"]);
-        }
+    if (query["createdAt[gte]"]) {
+      queryObj.createdAt.$gte = new Date(query["createdAt[gte]"]);
     }
 
-    // REMOVE SPECIAL FIELDS
-    delete query["createdAt[gte]"];
-    delete query["createdAt[lte]"];
-
-    const queryBuilder = new QueryBuilder(
-        User.find({ role: "CUSTOMER", isDeleted: true, ...queryObj }),
-        query
-    );
-
-    const usersData = queryBuilder
-        .filter()
-        .search(userSearchableFields)
-        .sort()
-        .fields()
-        .paginate();
-
-    const [data, meta] = await Promise.all([
-        usersData.build(),
-        queryBuilder.getMeta()
-    ])
-
-    return {
-        data,
-        meta
+    if (query["createdAt[lte]"]) {
+      queryObj.createdAt.$lte = new Date(query["createdAt[lte]"]);
     }
+  }
+
+  // REMOVE SPECIAL FIELDS
+  delete query["createdAt[gte]"];
+  delete query["createdAt[lte]"];
+
+  const queryBuilder = new QueryBuilder(
+    User.find({ role: "CUSTOMER", isDeleted: true, ...queryObj }),
+    query,
+  );
+
+  const usersData = queryBuilder
+    .filter()
+    .search(userSearchableFields)
+    .sort()
+    .fields()
+    .paginate();
+
+  const [data, meta] = await Promise.all([
+    usersData.build(),
+    queryBuilder.getMeta(),
+  ]);
+
+  return {
+    data,
+    meta,
+  };
 };
 
-const updateProfile = async (payload: Partial<IUser>, decodedToken: JwtPayload) => {
-    const user = await User.findById(decodedToken.userId);
-    if (!user) {
-        throw new AppError(httpStatus.NOT_FOUND, "User not found");
-    }
+const updateProfile = async (
+  payload: Partial<IUser>,
+  decodedToken: JwtPayload,
+) => {
+  const user = await User.findById(decodedToken.userId);
+  if (!user) {
+    throw new AppError(httpStatus.NOT_FOUND, "User not found");
+  }
 
-    if (payload.password) {
-        throw new AppError(httpStatus.FORBIDDEN, "You can't change your password here");
-    }
+  if (payload.password) {
+    throw new AppError(
+      httpStatus.FORBIDDEN,
+      "You can't change your password here",
+    );
+  }
 
-    const updatedUser = await User.findByIdAndUpdate(decodedToken.userId, payload, {
-        new: true,
-        runValidators: true,
-    });
-    return {
-        data: updatedUser
-    }
-}
+  const updatedUser = await User.findByIdAndUpdate(
+    decodedToken.userId,
+    payload,
+    {
+      new: true,
+      runValidators: true,
+    },
+  );
+  return {
+    data: updatedUser,
+  };
+};
 
 export const UserServices = {
-    createUserService,
-    getMe,
-    getSingleUser,
-    updateUser,
-    updateProfile,
-    getAllUsers,
-    getAllTrashUsers,
-    getAllCustomers,
-    getAllTrashCustomers,
-    getMyCustomers,
-    deleteUser
-}
+  createUserService,
+  getMe,
+  getSingleUser,
+  updateUser,
+  updateProfile,
+  getAllUsers,
+  getAllTrashUsers,
+  getAllCustomers,
+  getAllTrashCustomers,
+  getMyCustomers,
+  deleteUser,
+};
