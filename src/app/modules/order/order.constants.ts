2a3d7e5a546d1901dc405b798732a82af7e9fdee
ssmahim01 <mahimzayn666@gmail.com>
2026-04-30 20:25:45 +0600

    Added phone field in constant of user

--- src/app/modules/order/order.constants.ts
+++ src/app/modules/order/order.constants.ts
@@ -1,6 +1,7 @@
 export const orderSearchableFields = [
   "billingDetails.fullName",
   "billingDetails.email",
+  "billingDetails.phone",
   "billingDetails.address",
   "customOrderId",
   "orderStatus",
